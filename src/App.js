11ecdb69128338b0809538e266b4a4fed819aea8
myxsan <yksl3461@gmail.com>
2023-03-06 20:01:11 +0300

    q

--- src/App.js
+++ src/App.js
@@ -10,32 +10,106 @@ function App() {
           firstName: "",
           lastName: "",
           email: "",
+          gender: "male",
+          hobies: [],
+          country: "turkiye",
         }}
         onSubmit={async (values) => {
           console.log(values);
         }}
       >
-        {({ handleSubmit, handleChange }) => (
+        {({ handleSubmit, handleChange, values }) => (
           <form onSubmit={handleSubmit}>
             <label htmlFor="firstName">First Name</label>
-            <input name="firstName" onChange={handleChange}/>
+            <input
+              name="firstName"
+              value={values.firstName}
+              onChange={handleChange}
+            />
 
             <br />
             <br />
 
             <label htmlFor="lastName">Last Name</label>
-            <input name="lastName" onChange={handleChange}/>
+            <input
+              name="lastName"
+              value={values.lastName}
+              onChange={handleChange}
+            />
 
             <br />
             <br />
 
             <label htmlFor="email">Email</label>
-            <input name="email" onChange={handleChange}/>
+            <input name="email" value={values.email} onChange={handleChange} />
 
             <br />
             <br />
+
+            <span>Male</span>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              onChange={handleChange}
+              checked={values.gender === "male"}
+            />
+
+            <span>Female</span>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              onChange={handleChange}
+              checked={values.gender === "female"}
+            />
+
+            <br />
+            <br />
+
+            <div>
+              <input
+                type="checkbox"
+                name="hobies"
+                value="Cinema"
+                onChange={handleChange}
+              />
+              Cinema
+            </div>
+
+            <div>
+              <input
+                type="checkbox"
+                name="hobies"
+                value="Football"
+                onChange={handleChange}
+              />
+              Football
+            </div>
+
+            <div>
+              <input
+                type="checkbox"
+                name="hobies"
+                value="Photography"
+                onChange={handleChange}
+              />
+              Photography
+            </div>
+
+            <br />
+            
+            <select name="country" onChange={handleChange} value={values.country}>
+              <option value="turkiye">Turkiye</option>
+              <option value="uk">UK</option>
+              <option value="usa">USA</option>
+            </select>
+
+            <br /><br />
 
             <button type="submit">Submit</button>
+            <br />
+            <code>{JSON.stringify(values)}</code>
           </form>
         )}
       </Formik>
